718f4b5e133431c47cd0ab27fd7e77bbb9321de4
Coding4hours <138321129+Coding4Hours@users.noreply.github.com>
2026-01-21 21:02:02 -0800

    did some housekeeping

---
+++ src/global.d.ts
@@ -0,0 +1,3 @@
+declare module "cm-chessboard/src/Chessboard.js";
+declare module "cm-chessboard/src/extensions/arrows/Arrows.js";
+declare module "cm-chessboard/src/extensions/right-click-annotator/RightClickAnnotator.js";
